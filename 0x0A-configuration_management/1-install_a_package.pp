cde00bd076f5f033c4d0887286a928477e28bc67
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-24 08:26:31 -0800

    Alx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,16 @@
 # Install a package
+package { 'python3':
+  ensure   => '3.8.10'
+}
+
+package { 'python3-pip':
+  ensure   => installed
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
 
 package { 'flask':
   ensure   => '2.1.0',
