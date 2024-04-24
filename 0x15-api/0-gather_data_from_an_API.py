140936f7a8651e3897fbd01463e0a128c9d6978d
Abdullahi Husain <abdulmohusain@gmail.com>
2024-04-24 23:05:42 +0100

    Alx

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,27 +8,21 @@ import requests
 import sys
 
 
-def main():
-    """main method"""
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user_url = url + "users/{}".format(employee_id)
-    task_url = user_url + "/todos"
+employee_id = sys.argv[1]
+url = "https://jsonplaceholder.typicode.com/"
+user_url = url + "users/{}".format(employee_id)
+task_url = user_url + "/todos"
 
-    response_user = requests.get(user_url).json()
-    response_task = requests.get(task_url).json()
-    tasks_done = [task['title'] for task in response_task if task['completed']]
+response_user = requests.get(user_url).json()
+response_task = requests.get(task_url).json()
+tasks_done = [task['title'] for task in response_task if task['completed']]
 
-    print(
-        'Employee {} is done with tasks({}/{}):'.format(
-            response_user["name"],
-            len(tasks_done),
-            len(response_task)
-        )
+print(
+    'Employee {} is done with tasks({}/{}):'.format(
+        response_user["name"],
+        len(tasks_done),
+        len(response_task)
     )
-    for task in tasks_done:
-        print("\t {}".format(task))
-
-
-if __name__ == '__main__':
-    main()
+)
+for task in tasks_done:
+    print("\t {}".format(task))
